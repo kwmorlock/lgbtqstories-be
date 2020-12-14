8ead321f19f018ede59d56221efb9abb8c0ea985
kwmorlock <kmorlockcodes@gmail.com>
2020-12-14 17:06:22 -0500

    set: usersModel.js model exports

--- users/users/usersModel.js
+++ users/users/usersModel.js
@@ -1,1 +1,30 @@
-//setup
+const db = require("../../database/dbConfig");
+
+module.exports = {
+  add,
+  findUser,
+  findBy,
+  findById,
+};
+
+function findUser() {
+  return db("users").select("id", "username", "password", "email");
+}
+
+function findBy(filter) {
+  return db("users").where(filter);
+}
+
+async function add(user) {
+  try {
+    const [id] = await db("users").insert(user, "id");
+
+    return findById(id);
+  } catch (error) {
+    throw error;
+  }
+}
+
+function findById(id) {
+  return db("users").where({ id }).first();
+}
