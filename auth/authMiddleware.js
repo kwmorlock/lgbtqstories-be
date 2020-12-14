cfb985465de981f881e4ace9703ab6227745c651
kwmorlock <kmorlockcodes@gmail.com>
2020-12-14 14:01:55 -0500

    set: authMiddleware.js token auth

--- auth/authMiddleware.js
+++ auth/authMiddleware.js
@@ -1,1 +1,28 @@
-//setup
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (token) {
+    const secret = process.env.JWT_SECRET || "noneedforsecrets,tell the truth!";
+
+    jwt.verify(token, secret, (error, decodedToken) => {
+      if (error) {
+        res
+          .status(401)
+          .json({
+            message:
+              "No entry allowed with this method, please take this time to donate to your favorite LGBTQ charity!",
+          });
+      } else {
+        req.jwt = decodedToken;
+
+        next();
+      }
+    });
+  } else {
+    res
+      .status(400)
+      .json({ message: "Please provide the authentication information" });
+  }
+};
