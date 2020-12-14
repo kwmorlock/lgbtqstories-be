2768ce3f10fe878e74e163c54954441cbbb12ba7
kwmorlock <kmorlockcodes@gmail.com>
2020-12-13 21:02:54 -0500

    upd: knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,46 +1,22 @@
-// Update with your config settings.
+require("dotenv").config();
 
 module.exports = {
   development: {
     client: "sqlite3",
-    connection: { filename: "./database/lgbt.db3" },
     useNullAsDefault: true,
+    connection: {
+      filename: "./database/lgbtq.db3",
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
     migrations: {
       directory: "./database/migrations",
-      tableName: "dbmigrations",
     },
-    seeds: { directory: "./database/seeds" },
-  },
-
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
-  },
-
-  production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
+    seeds: {
+      directory: "./database/seeds",
     },
   },
 };
