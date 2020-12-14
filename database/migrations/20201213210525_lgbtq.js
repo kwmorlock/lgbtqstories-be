135ae0c6d53e8a7d172bdc5b04368feaf79332e5
kwmorlock <kmorlockcodes@gmail.com>
2020-12-14 08:13:37 -0500

    set: migration users

--- database/migrations/20201213210525_lgbtq.js
+++ database/migrations/20201213210525_lgbtq.js
@@ -1,5 +1,12 @@
 
 exports.up = function(knex) {
+    return knex.schema
+    .createTable("users", (tbl) => {
+      tbl.increments();
+      tbl.string("username", 50).index().notNullable().unique();
+      tbl.string("password", 20).notNullable();
+      tbl.string("email")
+    })
   
 };
 
