f1bda2311caf54e49adb7edead5d9d7f88fd4a4f
kwmorlock <kmorlockcodes@gmail.com>
2020-12-14 17:42:13 -0500

    set: storiesModel.js exports

--- stories/storiesrouter/storiesModel.js
+++ stories/storiesrouter/storiesModel.js
@@ -1,1 +1,55 @@
-//setup
+const db = require("../../database/dbConfig");
+
+module.exports = {
+  find,
+  findByUser,
+  findById,
+  add,
+  update,
+  destroy,
+};
+
+function find() {
+  return db("stories")
+    .join("users", "users.id", "stories.usersId")
+    .select("stories.id", "stories.title", "stories.story", "stories.tags");
+}
+
+function findById(id) {
+  return db("stories")
+    .join("users", "users.id", "stories.usersId")
+    .select("stories.id", "stories.title", "stories.story", "stories.tags")
+    .where("stories.id", id)
+    .first();
+}
+
+function add(stories) {
+  return db("stories")
+    .insert(stories, "id")
+    .then((id) => {
+      return findById(id);
+    });
+}
+
+function update(updates, id) {
+  return db("stories").where({ id }).update(updates);
+}
+
+function destroy(id) {
+  return db("stories").where({ id }).del();
+}
+
+function findByUser(usersId) {
+  return db("stories")
+    .join("users", "users.id", "stories.usersId")
+    .select(
+      "users.id",
+      "users.title",
+      "stories.story",
+      "stories.user",
+      "users.username",
+      "users.password",
+      "users.email"
+    )
+    .where("stories.UsersId", usersId);
+}
