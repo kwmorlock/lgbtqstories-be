477e3d056591aabab3779355c2c0f376e72f7242
kwmorlock <kmorlockcodes@gmail.com>
2020-12-13 19:24:14 -0500

    set: dbConfig.js knexconfig

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,1 +1,8 @@
-//setup
+const knex = require("knex");
+
+const knexconfig = require("../knexfile.js");
+const environment = process.env.NODE_ENV || "development";
+
+module.exports = knex(knexconfig[environment]);
+
+// ('development') this will break postma
