437dd4cc50e5057e048ce888528be969feb00d0c
kwmorlock <kmorlockcodes@gmail.com>
2020-12-14 17:57:28 -0500

    set: server.js user routers

--- api/server.js
+++ api/server.js
@@ -1,6 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const authenticate = require("../auth/authMiddleware");
+const usersAuthRouter = require("../auth/authUsersRouter");
+const storiesRouter = require("../stories/storiesrouter/storiesRouter");
+const usersRouter = require("../users/users/usersRouter");
 
 const server = express();
 
@@ -8,8 +13,12 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
+server.use("/api/users/users", authenticate, usersRouter);
+server.use("/api/users/auth", usersAuthRouter);
+server.use("/api/stories", authenticate, storiesRouter);
+
 server.get("/", (req, res) => {
-    res.json({ api: "Ready To Tell Stories!!!" });
-  });
+  res.json({ api: "Ready To Tell Stories!!!" });
+});
 
 module.exports = server;
