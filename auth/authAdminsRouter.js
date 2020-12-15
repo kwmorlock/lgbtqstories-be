ffa48d558deb2c21fe7efd9ad3e2fa39b6dcdd80
kwmorlock <kmorlockcodes@gmail.com>
2020-12-15 18:28:35 -0500

    set: authAdminsRouter.js crud

--- auth/authAdminsRouter.js
+++ auth/authAdminsRouter.js
@@ -1,1 +1,73 @@
-//setup
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const router = require("express").Router();
+
+const Admins = require("../users/admins/adminsModel");
+const { isValid } = require("../users/admins/adminsService");
+
+router.post("/register", (req, res) => {
+  const credentials = req.body;
+
+  if (isValid(credentials)) {
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+
+    const hash = bcryptjs.hashSync(credentials.password, rounds);
+
+    credentials.password = hash;
+
+    Admins.add(credentials)
+      .then((user) => {
+        res.status(201).json({ data: user });
+      })
+      .catch((error) => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "Please provide your fabulous username and password",
+    });
+  }
+});
+
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (isValid(req.body)) {
+    Admins.findBy({ username: username })
+      .then(([user]) => {
+        if (user && bcryptjs.compareSync(password, user.password)) {
+          const token = createToken(user);
+
+          res.status(200).json({ message: "You made it!", token });
+        } else {
+          res.status(401).json({ message: "Invalid credentials" });
+        }
+      })
+      .catch((error) => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "Please provide your fabulous username and password",
+    });
+  }
+});
+
+function createToken(user) {
+  const payload = {
+    sub: user.id,
+    username: user.username,
+    role: user.role,
+  };
+
+  const secret = process.env.JWT_SECRET || "noneedforsecrets,tell the truth!";
+
+  const options = {
+    expiresIn: "1d",
+  };
+
+  return jwt.sign(payload, secret, options);
+}
+
+module.exports = router;
