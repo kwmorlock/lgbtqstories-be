7cc8a365523883a7df13289892cc69ee4d9d60ec
kwmorlock <kmorlockcodes@gmail.com>
2020-12-15 16:54:42 -0500

    set: adminsModel.js crud

--- users/admins/adminsModel.js
+++ users/admins/adminsModel.js
@@ -1,1 +1,30 @@
-//setup
+const db = require("../../database/dbConfig");
+
+module.exports = {
+  add,
+  findAdmin,
+  findBy,
+  findById,
+};
+
+function findAdmin() {
+  return db("admins").select("id", "username", "password", "email");
+}
+
+function findBy(filter) {
+  return db("admins").where(filter);
+}
+
+async function add(user) {
+  try {
+    const [id] = await db("admins").insert(user, "id");
+
+    return findById(id);
+  } catch (error) {
+    throw error;
+  }
+}
+
+function findById(id) {
+  return db("admins").where({ id }).first();
+}
