2b9bb686ffed13f76848ac1891c4c9868da3e04d
kwmorlock <kmorlockcodes@gmail.com>
2020-12-15 17:53:37 -0500

    set: notesModel.js crud exports

--- stories/notesrouter/notesModel.js
+++ stories/notesrouter/notesModel.js
@@ -1,1 +1,74 @@
-//setup
+const db = require("../../database/dbConfig");
+
+module.exports = {
+  find,
+  findByAdmin,
+  findById,
+  add,
+  update,
+  remove,
+};
+
+function find() {
+  return db("adminNote")
+    .join("stories", "adminNote.storyId", "stories.id")
+    .join("admins", "adminNote.adminsId", "admins.id")
+    .select(
+      "adminNote.id",
+      "stories.title",
+      "stories.story",
+      "stories.tags",
+      "adminNote.note",
+      "adminNote.adminsId",
+      "admins.username"
+    );
+}
+
+function update(updates, id) {
+  return db("adminNote").where({ id }).update(updates);
+}
+
+function findById(id) {
+  return db("adminNote")
+    .join("stories", "adminNote.storyId", "stories.id")
+    .join("admins", "adminNote.adminsId", "admins.id")
+    .select(
+      "adminNote.id",
+      "stories.title",
+      "stories.story",
+      "stories.tags",
+      "adminNote.note",
+      "adminNote.adminsId",
+      "admins.username"
+    )
+    .where("adminNote.id", id)
+    .first();
+}
+
+function add(adminNotes) {
+  return db("adminNote")
+    .insert(adminNotes, "id")
+    .then((ids) => {
+      return findById(ids);
+    });
+}
+
+function remove(id) {
+  return db("adminNote").where({ id }).del();
+}
+
+function findByAdmin(id) {
+  return db("adminNote")
+    .join("stories", "adminNote.storyId", "stories.id")
+    .join("admins", "adminNote.adminsId", "admins.id")
+    .select(
+      "adminNote.id",
+      "stories.title",
+      "stories.story",
+      "stories.tags",
+      "adminNote.note",
+      "adminNote.adminsId",
+      "admins.username"
+    )
+    .where("adminNote.adminsId", id);
+}
