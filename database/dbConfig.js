8f6e9a380cd9d4ee4e40fea5b7fbc30e83331252
kwmorlock <kmorlockcodes@gmail.com>
2020-12-16 20:39:00 -0500

    upd: dbconfig development change two

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -3,6 +3,7 @@ const knex = require("knex");
 const knexconfig = require("../knexfile.js");
 // const environment = process.env.NODE_ENV || "development";
 
-module.exports = knex(knexconfig[environment]);
+// module.exports = knex(knexconfig[environment]);
+module.exports = knex(knexconfig[development]);
 
-// ('development') this will break postma
+// ('development') this will break postman
