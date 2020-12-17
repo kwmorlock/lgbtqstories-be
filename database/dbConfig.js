2c4f8152a728c208ace77fbff0de53f140c0248d
kwmorlock <kmorlockcodes@gmail.com>
2020-12-16 20:35:25 -0500

    upd: dbConfid.js development

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,7 +1,7 @@
 const knex = require("knex");
 
 const knexconfig = require("../knexfile.js");
-const environment = process.env.NODE_ENV || "development";
+// const environment = process.env.NODE_ENV || "development";
 
 module.exports = knex(knexconfig[environment]);
 
